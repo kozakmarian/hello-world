33f3c10c1f730fe864c2f3469b33bd4dbb49568d
student <student@158.197.35.89>
2016-09-20 08:14:07 +0200

    Odstranovanie komentarov

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,17 @@
+
+package helloworld;
+
+/**
+ *
+ * @author student
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
